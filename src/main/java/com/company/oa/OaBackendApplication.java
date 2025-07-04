b63141fa561d95e2b01f862e508907cacfca0b1f
ChrisYou666 <2265975667@qq.com>
2025-07-04 23:30:47 +0800

    chore: 初始化项目骨架

---
+++ src/main/java/com/company/oa/OaBackendApplication.java
@@ -0,0 +1,11 @@
+package com.company.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OaBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/company/oa/OaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
